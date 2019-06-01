0a79cb104ad055029aa0f26bb5442042ecee9037
daniel_ceptinel <daniel.alvarez@ceptinel.com>
2019-06-01 16:45:54 -0400

    Welcome screen

---
+++ lib/welcome.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import './drawer.dart';
+import './login.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Mentor match'),
+      // ),
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text("Welcome to mentor match -  flutter app"),
+                SizedBox(width: 25),
+                SizedBox(
+                  child: Image.asset(
+                    "assets/images/flutter-icon.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                SizedBox(width: 25),
+                Row(
+                  children: <Widget>[
+                    Expanded(child: RaisedButton(
+                        child: Text('Sign in'),
+                        onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => LoginPage()),
+                            );
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 25),
+                    Expanded(child: RaisedButton(
+                        child: Text('Sign up'),
+                        onPressed: () {
+                          // Navigate to second route when tapped.
+                        },
+                      ),
+                    )
+                  ],
+                ),
+                
+              ],
+            ),
+          ),
+        ),
+      ),
+      // drawer: MyDrawer(),
+    );
+  }
+}
